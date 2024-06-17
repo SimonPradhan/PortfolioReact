7bf6bb007bae8a481ded6f3b4c5f03d72024c5c0
SimonPradhan <simon234pradhan@gmail.com>
2024-06-17 19:38:19 +0545

    change in links and animations

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
-import {motion} from 'framer-motion'
 
 export default function Contact() {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <motion.h1 
-      whileInView={{ opacity: 1, y: 0}}
-      initial= {{ opacity: 0, y: -100}}
-      transition={{ duration: 0.5}}
-      className="my-10 text-center text-4xl">Get In Touch</motion.h1>
+      <h1 className="my-10 text-center text-4xl">Get In Touch</h1>
         <div className="text-center tracking-tighter">
-            <motion.p
-            whileInView={{ opacity: 1, x: 0}}
-            initial= {{ opacity: 0, x: -100}}
-            transition={{ duration: 1}}            
-             className="my-4">Hadigaun-5, Kathmandu</motion.p>
-            <motion.p
-            whileInView={{ opacity: 1, x: 0}}
-            initial= {{ opacity: 0, x: 100}}
-            transition={{ duration: 1}}   
-             className="my-4">+977-9828753172</motion.p>
+            <p className="my-4">Hadigaun-5, Kathmandu</p>
+            <p className="my-4">+977-9828753172</p>
             <a href="#" className="border-b">
                 simon234pradhan@gmail.com
             </a>
